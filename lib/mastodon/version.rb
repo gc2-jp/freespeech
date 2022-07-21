96c1a0dab8994a5cc0c04e7e37672ec40511d033
freespeech-gaac2 <freespeech.gaac2@gmail.com>
2022-07-21 11:37:32 +0000

    lib/mastodon/version.rb commit

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -33,7 +33,7 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY', 'mastodon/mastodon')
+      ENV.fetch('GITHUB_REPOSITORY', 'freespeech-gaac2/freespeech')
     end
 
     def source_base_url
