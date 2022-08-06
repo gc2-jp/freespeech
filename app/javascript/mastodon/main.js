4286b7daf52ed080a1affe4b476689f2db82c0e1
freespeech-gaac2 <freespeech.gaac2@gmail.com>
2022-08-06 04:27:24 +0000

    app/javascript/mastodon/main.js, app/views/home/index.html.haml, config/initializers/content_security_policy.rb, yarn.lock commit

--- app/javascript/mastodon/main.js
+++ app/javascript/mastodon/main.js
@@ -42,23 +42,57 @@ const sleep = function(msec){
   })
 };
 
-(async () => {
-  let ComposeFormList = document.getElementsByClassName("compose-form");
-  while (ComposeFormList.length == 0){
-    await sleep(0.1);
-  }
-  let AdElement = document.createElement("div");
-  AdElement.id = "ad_0";
-  AdElement.classList.add('EleAd0');
-  AdElement.innerHTML = `
-<div class='EleAd0_inner'>
-  <div id="85a600a0b51da95f7634f2b99bb31b42" >
-  </div>
-</div>
-`;
-  ComposeFormList[0].appendChild(AdElement);
-})();
 
+
+const AdList = {
+  "ad_1":[
+    {
+      "pctag":`
+        <div class='EleAd1_inner'>
+         <iframe id='a2e8f4e0' name='a2e8f4e0' src='https://ad.gc2.jp/www/delivery/afr.php?zoneid=2&amp;cb=INSERT_RANDOM_NUMBER_HERE' frameborder='0' scrolling='no' width='300' height='250' allow='autoplay'><a href='https://ad.gc2.jp/www/delivery/ck.php?n=aa328861&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='https://ad.gc2.jp/www/delivery/avw.php?zoneid=2&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=aa328861' border='0' alt='' /></a></iframe>
+         @Ad
+        </div>
+      `,
+      "sptag":`
+        <div class='EleAd1_inner'>
+          <iframe id='ad05adf8' name='ad05adf8' src='https://ad.gc2.jp/www/delivery/afr.php?zoneid=4&amp;cb=INSERT_RANDOM_NUMBER_HERE' frameborder='0' scrolling='no' width='300' height='250' allow='autoplay'><a href='https://ad.gc2.jp/www/delivery/ck.php?n=af9f3ae1&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='https://ad.gc2.jp/www/delivery/avw.php?zoneid=4&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=af9f3ae1' border='0' alt='' /></a></iframe>
+          @Ad
+        </div>
+      `,
+      "EleAd":null,
+    },
+    {
+      "pctag":`
+        <div class='EleAd1_inner'>
+          <iframe id='aab6f898' name='aab6f898' src='https://ad.gc2.jp/www/delivery/afr.php?zoneid=3&amp;cb=INSERT_RANDOM_NUMBER_HERE' frameborder='0' scrolling='no' width='300' height='250' allow='autoplay'><a href='https://ad.gc2.jp/www/delivery/ck.php?n=aeca92ee&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='https://ad.gc2.jp/www/delivery/avw.php?zoneid=3&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=aeca92ee' border='0' alt='' /></a></iframe>
+          @Ad
+        </div>
+      `,
+      "sptag":`
+        <div class='EleAd1_inner'>
+          <iframe id='aece51c1' name='aece51c1' src='https://ad.gc2.jp/www/delivery/afr.php?zoneid=6&amp;cb=INSERT_RANDOM_NUMBER_HERE' frameborder='0' scrolling='no' width='300' height='250' allow='autoplay'><a href='https://ad.gc2.jp/www/delivery/ck.php?n=ab7b2dc2&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='https://ad.gc2.jp/www/delivery/avw.php?zoneid=6&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=ab7b2dc2' border='0' alt='' /></a></iframe>
+          @Ad
+        </div>
+      `,
+       "EleAd":null,
+    },
+    {
+      "pctag":`
+        <div class='EleAd1_inner'>
+          <iframe id='ad05adf8' name='ad05adf8' src='https://ad.gc2.jp/www/delivery/afr.php?zoneid=4&amp;cb=INSERT_RANDOM_NUMBER_HERE' frameborder='0' scrolling='no' width='300' height='250' allow='autoplay'><a href='https://ad.gc2.jp/www/delivery/ck.php?n=af9f3ae1&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='https://ad.gc2.jp/www/delivery/avw.php?zoneid=4&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=af9f3ae1' border='0' alt='' /></a></iframe>
+          @Ad
+        </div>
+      `,
+      "sptag":`
+        <div class='EleAd1_inner'>
+          <iframe id='ae7fc7d0' name='ae7fc7d0' src='https://ad.gc2.jp/www/delivery/afr.php?zoneid=7&amp;cb=INSERT_RANDOM_NUMBER_HERE' frameborder='0' scrolling='no' width='300' height='250' allow='autoplay'><a href='https://ad.gc2.jp/www/delivery/ck.php?n=a84994b6&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='https://ad.gc2.jp/www/delivery/avw.php?zoneid=7&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a84994b6' border='0' alt='' /></a></iframe>
+          @Ad
+        </div>
+      `,
+       "EleAd":null,
+    },
+  ],
+};
 (async () => {
   let TweetListEleList = document.getElementsByClassName("item-list");
   while (TweetListEleList.length == 0){
@@ -69,30 +103,59 @@ const sleep = function(msec){
     await sleep(0.1);
   }
 
-  let AdIndex = 0;
-  TweetList = Array.from(TweetList);
-  let count = 0;
-  for (let index in TweetList){
-    let EleTweet = TweetList[index];
 
-    if (EleTweet.tagName.toLowerCase() != "article"){
-      continue;
+
+  //初期生成
+  for (let index in AdList["ad_1"]){
+    let AdData = AdList["ad_1"][index];
+
+    let EleAd = document.createElement("div");
+    EleAd.id = `ad_1_${index}`;
+    EleAd.classList.add(`EleAd1`);
+    if (window.innerWidth > 895){
+     EleAd.innerHTML = AdData["pctag"];
+    }else{
+      EleAd.innerHTML = AdData["sptag"];
     }
-
-    if (count%10 == 0){
-      let AdElement = document.createElement("div");
-      AdElement.id = `ad_1_${AdIndex}`;
-      AdElement.classList.add(`EleAd1`);
-      AdElement.innerHTML = `
-<div class='EleAd1_inner'>
-  <div id="88e27918ca6c4caf07dbe7db0898257f" >
-  </div>
-</div>
-`;
-      EleTweet.before(AdElement);
-
-      AdIndex = AdIndex + 1;
-    }
-    count = count + 1;
+    AdData["EleAd"] = EleAd;
   }
+
+  while (true){
+    try{
+      let AdIndex = 0;
+      let TweetListEleList = document.getElementsByClassName("item-list");
+      let TweetList = Array.from((TweetListEleList[0]).children);
+      let count = 0;
+      for (let index in TweetList){
+        let EleTweet = TweetList[index];
+
+        if (EleTweet.tagName.toLowerCase() != "article"){
+          continue;
+        }
+
+        if (count%10 == 0){
+          EleTweet.before(AdList["ad_1"][AdIndex]["EleAd"]);
+          AdIndex = AdIndex + 1;
+          if (AdList["ad_1"].length <= AdIndex){
+            break;
+          }
+        }
+        count = count + 1;
+      }
+
+      count = 0;
+      while (count < 60){
+        await sleep(1);
+        let AdEleList = document.getElementsByClassName("EleAd1");
+        if (AdEleList.length == 0 && count >= 1){
+          break;
+        }
+        count = count + 1;
+      }
+    }catch(e){
+      console.log(e);
+    }finally{
+      await sleep(0.1);
+    };
+  };
 })();
